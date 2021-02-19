708fd28d245303c8cd57aa5f58bc3046d691c4e3
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-19 12:45:54 -0600

    boolean conversion for task_completed is functioning

--- api/task/model.js
+++ api/task/model.js
@@ -3,12 +3,14 @@ const db = require("../../data/dbConfig.js")
 
 const get = () =>{
     return db("tasks")
+
 }
 const createTask = (newTask)=>{
     return db("tasks").insert(newTask)
         .then(([id])=>{
             return db("tasks").where("task_id",id).first();
         })
+    
 }
 
 module.exports={
--- api/task/router.js
+++ api/task/router.js
@@ -6,6 +6,13 @@ const router = express.Router();
 router.get('/', async (req,res)=>{
     try{
         const data = await Tasks.get()
+        data.map(task=>{
+            if(task.task_completed===0){
+                return task.task_completed = false
+            } else{
+                return task.task_completed = true
+            }
+        })
         res.status(200).json(data)
     }catch(error){
         res.status(400).json(error.message)
