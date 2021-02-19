f981a37c9c5176108bc2a4fbf51ca4a16d75f371
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-19 11:21:11 -0600

    server set up, spinning in postman correctly

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
-// build your server here and require it from index.js
+
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+server.get('/',(req,res)=>{
+    res.status(200).json("hello bubblegum")
+})
+
+module.exports=server;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// start your server here
+
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 3333;
+
+server.listen(PORT, ()=>{
+    console.log(`Server listening on port:${PORT}`)
+})
